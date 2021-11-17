9891b26529b4c84dfbfbae58bdded92cdfd34b46
Sobyn <evenfosser95@gmail.com>
2021-11-17 21:15:25 +0100

    player controller

---
+++ ArkanoIId/Assets/Scripts/vaus.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vaus : MonoBehaviour
+{
+    public float movementSpeed = 150;
+
+    private void FixedUpdate() {
+        float horizontal = Input.GetAxisRaw("Horizontal");     //  Get the player's horizontal input
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * horizontal * movementSpeed;
+    }
+}
