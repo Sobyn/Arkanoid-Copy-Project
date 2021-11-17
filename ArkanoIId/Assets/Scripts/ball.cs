09edcdd0e5c764ac2b9b99a49f495e6c4d21bbdb
Sobyn <evenfosser95@gmail.com>
2021-11-17 23:11:52 +0100

    hitsounds and reset ball position when losing

--- ArkanoIId/Assets/Scripts/ball.cs
+++ ArkanoIId/Assets/Scripts/ball.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class ball : MonoBehaviour
 {
     public float movementSpeed = 100f;
+    public AudioSource hitSound;
+    public GameObject playerObject;
+    private Vector2 ballPosition;
     float hitLocation(Vector2 ballPos, Vector2 vausPos, float vausWidth){
-        return(ballPos.x - vausPos.x) / vausWidth;
-    }
+        return(ballPos.x - vausPos.x) / vausWidth;}
 
     void Start()
         {GetComponent<Rigidbody2D>().velocity = Vector2.up * movementSpeed;}
 
     private void OnCollisionEnter2D(Collision2D other) {
-        
         if(other.gameObject.name == "vaus"){
             float x = hitLocation(transform.position, other.transform.position, other.collider.bounds.size.x);
-
             Vector2 direction = new Vector2(x,1).normalized;
-
             GetComponent<Rigidbody2D>().velocity = direction * movementSpeed;
+        }
+
+        hitSound.Play();
+    }
+
+    private void Update() {
+        if(transform.position.y < -98){
+            ballPosition.x = playerObject.transform.position.x;
+            ballPosition.y = -90;
+            transform.position = ballPosition;
         }
     }
 }
