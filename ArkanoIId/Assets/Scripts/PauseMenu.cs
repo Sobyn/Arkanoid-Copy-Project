df0ef77460f6e3baa846783fdfc831a5058b2d11
Sobyn <evenfosser95@gmail.com>
2021-11-18 03:24:26 +0100

    main menu and pause menu implemented

---
+++ ArkanoIId/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(gameIsPaused){
+                Resume();
+            }
+
+            else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame(){
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
